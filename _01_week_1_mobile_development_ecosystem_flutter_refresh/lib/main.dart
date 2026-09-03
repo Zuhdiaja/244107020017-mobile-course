ca3f0c2b6275e73516e8e859a3b97133bc559a11
Muhammad Zuhdi Yudadharma <yudazuhdi21@gmail.com>
2026-09-03 13:20:14 +0700

    feat: create week 1 Flutter profile app

---
+++ _01_week_1_mobile_development_ecosystem_flutter_refresh/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Profil Mahasiswa',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Profil Mahasiswa'),),
+        body: const Center(
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            Icon(Icons.school, size: 72),
+            SizedBox(height: 16),
+            Text('Muhammad Zuhdi Yudadharma', style: TextStyle(fontSize: 24)),
+            Text('NIM: 244107020017'),
+              Text('Pemrograman Mobile — Minggu 1'),
+              Text('Politeknik Negeri Malang'),
+          ]),
+        ),
+      ),
+    );
+  }
+}
